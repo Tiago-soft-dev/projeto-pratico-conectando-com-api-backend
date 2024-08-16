74d33b453ebf64786ad109e1db43940417dc1451
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-15 22:11:43 -0300

    Adicionada biblioteca de notificações `React Toastify`.

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,41 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import { Api } from './Api'
 
 
 
 function App() {
-  const notify = () => {
-    toast('testando toastify')
+  const notify = {
+    delete: () => toast('Removido')
   }
 
   const [devmons, setDevmons] = useState([])
-  // console.log(15,devmons);
-  // const dados = Api.getDataApi()
-  // setDevmons(dados)
 
 
-  // const apiUrl = 'https://backend-integrar-com-o-frontend.onrender.com/personagem'
 
-
-  // async function getDataApi() {
-
-  //   const response = await fetch(apiUrl)
-  //   const data = await response.json()
-  //   setDevmons(data)
-  // }
-
-
-  useEffect(() => { 
-    const fetchData = async ()=>{
+  useEffect(() => {
+    const fetchData = async () => {
       const data = await Api.getDataApi()
       setDevmons(data)
     }
     fetchData()
-   }, [])
+  }, [])
 
   const deleteComponent = (id) => {
-    const data = devmons.filter((devmon)=>devmon._id !== id)
+    const data = devmons.filter((devmon) => devmon._id !== id)
     setDevmons(data)
+    notify.delete('oi')
   }
 
   return (
     <>
       <div className='container'>
-        <ToastContainer />
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
         {devmons.map((devmon, index) => (
-          
-            <Card key={devmon._id} item={devmon} index={index} deleteComponent={deleteComponent}/>
+
+          <Card key={devmon._id} item={devmon} index={index} deleteComponent={deleteComponent} />
         ))}
 
       </div>
