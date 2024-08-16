c9163df1849435b79a019bca9ec80318d6cba4d8
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-15 21:40:37 -0300

    Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,59 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
+import { toast, ToastContainer } from 'react-toastify'
+import { Api } from './Api'
 
 
 
 function App() {
-
-const [devmons, setDevmons] = useState([])
-
-
-  const apiUrl = 'https://backend-integrar-com-o-frontend.onrender.com/personagem'
-
-
-  async function getDataApi() {
-
-    
-    const response = await fetch(apiUrl)
-    const data = await response.json()
-   
-    devmons.push(...data)
-    
+  const notify = () => {
+    toast('testando toastify')
   }
- getDataApi()
- 
+
+  const [devmons, setDevmons] = useState([])
+  // console.log(15,devmons);
+  // const dados = Api.getDataApi()
+  // setDevmons(dados)
+
+
+  // const apiUrl = 'https://backend-integrar-com-o-frontend.onrender.com/personagem'
+
+
+  // async function getDataApi() {
+
+  //   const response = await fetch(apiUrl)
+  //   const data = await response.json()
+  //   setDevmons(data)
+  // }
+
+
+  useEffect(() => { 
+    const fetchData = async ()=>{
+      const data = await Api.getDataApi()
+      setDevmons(data)
+    }
+    fetchData()
+   }, [])
+
+  const deleteComponent = (id) => {
+    const data = devmons.filter((devmon)=>devmon._id !== id)
+    setDevmons(data)
+  }
 
   return (
-   <>
-    <div className='container'>
-      
-      {devmons.map((devmon)=>{
-        return <Card key={devmon._id} item={devmon}/>
-      })}
-    </div>
-   </>
+    <>
+      <div className='container'>
+        <ToastContainer />
+        {devmons.map((devmon, index) => (
+          
+            <Card key={devmon._id} item={devmon} index={index} deleteComponent={deleteComponent}/>
+        ))}
+
+      </div>
+    </>
   )
+
 }
 
 export default App
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,14 @@
-
+import React from 'react'
+import './Card.css'
 
 export default function Card(props){
    
-    console.log(props);
-    
-    
     return (
         <div className="card">
            <div className="card-nome">{props.item.nome}</div>
            <div className="card-image"><img src={props.item.imagem}/></div>
-           <div className="card-evoluipara">{props.evoluiPara == null || props.evoluiPara == undefined ?(<>Não evolui!</>) : (<>Evolui para: {props.item.nome}</>)}</div>
+           <div className='card-evoluipara'>{props.item.evoluiPara == null || props.item.evoluiPara == undefined ?(<>Não evolui!</>) : (<>Evolui para: {props.item.nome}</>)}</div>
+           <button onClick={()=>props.deleteComponent(props.item._id)}>deletar</button>
            
         </div>
     )
